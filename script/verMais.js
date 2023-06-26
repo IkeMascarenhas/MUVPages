85f72a963fa332f44b9303175b9faaf102caa179
IkeMascarenhas <oikemascarenhas@gmail.com>
2023-06-26 09:14:58 -0300

    acabamento-1

--- script/verMais.js
+++ script/verMais.js
@@ -9,3 +9,14 @@ showAd.addEventListener('click', ()=>{
 
 })
 
+let avaHidden = [...document.querySelectorAll('.hide')]
+let showAva = document.querySelector('#seeAva')
+
+showAva.addEventListener('click', ()=>{
+    showAva.remove()
+    avaHidden.map((ad)=>{
+        ad.classList.toggle('show-ads')
+    })
+
+})
+
