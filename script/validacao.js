6e2bc0db319ebf536b3b0f07cbec72496afd473e
Kaique <130607210+IkeMascarenhas@users.noreply.github.com>
2023-09-27 15:46:33 -0300

    validação(cadastro)-e-atualizacao_mascaras

--- script/validacao.js
+++ script/validacao.js
@@ -1,24 +1,69 @@
 //*Variáveis------------------------
 const btn_cadastro = document.querySelector('#cadastrar-bttn')
-let age = document.querySelector('#data')
+if(document.querySelector('#data')){
+    const age = document.querySelector('#data')
+}else{
+    const age = false
+}
 let nowadays = new Date()
 console.log(nowadays.getSeconds())
+
 //*---------------------------------
 
-//!Validação idade do usuário--------------------------------------------
-btn_cadastro.addEventListener('click', ()=>{
-    let data_number = Date.parse(age.value)
-    let user_birth = new Date(data_number)
-    console.log(user_birth.getMilliseconds())
-    let full_date = [
-        user_birth.getDate()+1 <10?user_birth.getDate(): user_birth.getDate()+1,
-        user_birth.getMonth()+1 <10? user_birth.getMonth(): user_birth.getMonth()+1, 
-        user_birth.getFullYear()
-    ]
-    if(nowadays.getTime() - user_birth.getTime()  <= 567648000000){
-        alert("Você não pode concluir o cadastro pois é menor de idade!")
-    }
-})
-//! ----------------------------------------------------------------------
 
+// Função para validar o formulário
+btn_cadastro.addEventListener('click',(event)=>{
+     // Evitar o envio padrão do formulário
+     event.preventDefault();
+  
+     // Obter os valores dos campos
+     if(document.getElementById('nome')){
+     var nome = document.getElementById('nome').value;
+     }
+     let email = document.querySelector('input[type=email]').value;
+     if(document.querySelector('input[type=password')){
+     var senha = document.querySelector('input[type=password').value;
+     }
+
+ 
+   
+     // Realizar validação dos campos
+     if (nome === '' || email === '' || senha === '') {
+       alert('Por favor, preencha todos os campos.');
+       return false; // Impede o envio do formulário
+     }
+   
+     // Validar o formato do email usando uma expressão regular simples
+     var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+     if (!emailRegex.test(email)) {
+       alert('Por favor, insira um endereço de email válido.');
+       return false; // Impede o envio do formulário
+     }
+   
+     //!Validação idade do usuário--------------------------------------------
+     if(age){
+        let data_number = Date.parse(age.value)
+        console.log(age.value)
+        let user_birth = new Date(data_number)
+        console.log(user_birth.getMilliseconds())
+        if(nowadays.getTime() - user_birth.getTime()  <= 567648000000){
+            alert("Você não pode concluir o cadastro pois é menor de idade!")
+           return false
+       }
+     }
+     
+ //! ----------------------------------------------------------------------
+ 
+     // Se todos os campos estiverem válidos, você pode permitir o envio do formulário
+     document.querySelector('form').submit();
+     //Descomente esta linha para enviar o formulário
+   
+     // Ou você pode executar qualquer outra ação desejada aqui
+   
+     return true; // Permite o envio do formulário
+   
+   // Adicione um ouvinte de eventos para o formulário
+ //   document.getElementById('meuFormulario').addEventListener('submit', validarFormulario);
+})
+   
 
